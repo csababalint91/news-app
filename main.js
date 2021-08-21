e9aa31ca728c083100c87d412401b83567872828
Csaba Balint <csababalint@Csabas-MacBook-Pro.local>
2021-08-21 16:40:03 +0100

    Add more styleing, back to the top function and loading indicator

--- main.js
+++ main.js
@@ -2,52 +2,107 @@ const key = "41b766d0cd0542d1b34a4ab70409abdf";
 
 // Get Headlines
 
-const getHeadlinesBtn = document.getElementById("getHeadlinesBtn");
+const articleWrapper = document.querySelectorAll(".article-wrapper");
+const loader = document.querySelector('.loader');
 
 const getHeadlines = () => {
+  // Clear page
   newsListContainer.innerHTML = "";
+  headlinesContainer.innerHTML = "";
+  loader.style.display = 'block';
 
-
-  let topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=20&category=&apiKey=${key}`;
-
+  let topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=&apiKey=${key}`;
 
   fetch(topHeadlinesUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       const headlinesContainer = document.getElementById("headlinesContainer");
       let articleCard = data.articles
-        .map((article) => {
+        .map((article) => {    
           return `
-      <a href="${article.url}" target="_blank">
-        <div class="article-card">
-          <img src="${article.urlToImage}" href=${article.url} alt="article-img">
-          <h3>${article.title}</h3>
-          <p>${article.description}</p>
-          <cite style=>${article.source.name}-${article.publishedAt}</cite>
+      <div onclick="location.href ='${article.url}';" target="_blank" class="article-card">
+          <div class="img-wrapper">
+          <img src="${article.urlToImage || './no-image.jpeg'}" href=${article.url} alt="article-img" loading="lazy">
           </div>
-      </a>`;
-        })
-        .join("");
-
+          <h3>${article.title}</h3>
+          <p>${article.description || '--Description not available!--'}</p>
+          <cite>${article.source.name}-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth()+1}-${new Date(article.publishedAt).getDate()}</cite>
+      </div>`;
+    })
+    .join("");
+      
+      loader.style.display = 'none';
+    
       headlinesContainer.innerHTML = articleCard;
+
       
     })
     .catch((error) => {
       console.log(error);
     });
-};
+  };
+  
 
-getHeadlinesBtn.addEventListener("click", getHeadlines);
+//  Headlines by category
+
+const getValue = (e) => {
+  
+  // Clear page
+  newsListContainer.innerHTML = "";
+  headlinesContainer.innerHTML = "";
+  loader.style.display = 'block';
+
+  let categoriesValue =  e.target.value;  
+  
+  let categoriesUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=${categoriesValue}&apiKey=${key}`
+  
+
+  fetch(categoriesUrl)
+  .then(response => response.json())
+  .then(data => {
+    let categoryCard = data.articles
+    .map((article) => {
+      return `
+  <div onclick="location.href ='${article.url}';" target="_blank" class="article-card">
+      <div class="img-wrapper">
+      <img src="${article.urlToImage || './no-image.jpeg'}"  alt="article-img" loading="lazy">
+      </div>
+      <h3>${article.title}</h3>
+      <p>${article.description}</p><cite style=>${article.source.name}-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth()+1}-${new Date(article.publishedAt).getDate()}</cite>
+  </div>`;
+    })
+    .join("");
+    
+    loader.style.display = 'none';
+    headlinesContainer.innerHTML = categoryCard;
+
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+
+categories.addEventListener('change', getValue);
+
+//
+
 
 // Get Search
 
 const searchFrom = document.getElementById("searchFrom");
-const searchInput = document.getElementById("searchInput");
 const newsListContainer = document.getElementById("newsListContainer");
 
 const retriveSearch = (e) => {
+
+  // Clear page
+  newsListContainer.innerHTML = "";
   headlinesContainer.innerHTML = "";
+  loader.style.display = 'block';
+
+  // Set drop down value back to disabled
+  categories.value = "Select Category"
+
 
   e.preventDefault();
 
@@ -57,71 +112,51 @@ const retriveSearch = (e) => {
   fetch(searchUrl)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
       let searchList = data.articles
         .map(article => {
           return `
-      <a href="${article.url}" target="_blank">
-        <div class="search-list-item">
-          <img src="${article.urlToImage}" href=${article.url} alt="article-img">
-          <h1>${article.title}</h1>
-          </div>
-      </a>
-      <hr>`
+      <div onclick="location.href ='${article.url}';" target="_blank" class="search-list-item" >
+          <img src="${article.urlToImage || './no-image.jpeg'}" alt="article-img" loading="lazy"><cite style=>${article.source.name}-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth()+1}-${new Date(article.publishedAt).getDate()}</cite>
+          <h2>${article.title}</h2>  
+      </div>`
         })
         .join("");
+
+      loader.style.display = 'none'; 
       newsListContainer.innerHTML = searchList;
+
     });
-  console.log(topic);
 };
 
 searchFrom.addEventListener("submit", retriveSearch);
+//
 
 
-//  Headlines by category
+// Back to the top button
+window.addEventListener('scroll', () => {
 
-// const getValue = (e) => {
+  let crolled;
+  scrolled = window.scrollY;
+  const arrowTopBtn = document.getElementById('arrowTopBtn');
+  if(scrolled > 167) {
+    arrowTopBtn.style.visibility = 'visible'
+  } else {
+    arrowTopBtn.style.visibility = 'hidden'
+  }
+})
 
-//   let selectedValue = document.getElementById('categories').value;
-//   console.log(selectedValue);
-// }
+arrowTopBtn.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
 
-
-const getValue = (e) => {
-
-  newsListContainer.innerHTML = "";
-  
-  let categoriesValue =  e.target.value;  
-  
-  let categoriesUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=${categoriesValue}&apiKey=${key}`
-  
-  const categoriesListContainer = document.getElementById('categoriesListContainer');
-
-  fetch(categoriesUrl)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    let categoryCard = data.articles
-    .map((article) => {
-      return `
-  <a href="${article.url}" target="_blank">
-    <div class="article-card">
-      <img src="${article.urlToImage}" href=${article.url} alt="article-img">
-      <h3>${article.title}</h3>
-      <p>${article.description}</p>
-      </div>
-  </a>`;
-  
-    })
-    .join("");
-    
-  
-    headlinesContainer.innerHTML = categoryCard;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-}
+})
+//
 
 
-categories.addEventListener('change', getValue);
+// Loader 
+
+
+
+
+
+
+
