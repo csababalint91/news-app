352477092f2053406616841931843d504f7e5130
Csaba Balint <csababalint@csabas-mbp-2.home>
2021-12-12 19:39:17 +0000

    update api base url

--- main.js
+++ main.js
@@ -1,92 +1,103 @@
 const key = "41b766d0cd0542d1b34a4ab70409abdf";
 
+const newsAppApiBaseUrl = "https://news-app-api-zeta.vercel.app/";
+
 // Get Headlines
 
 const articleWrapper = document.querySelectorAll(".article-wrapper");
-const loader = document.querySelector('.loader');
+const loader = document.querySelector(".loader");
+const totalResults = document.getElementById('totalResult');
 
-const getHeadlines = () => {
+window.onload = getHeadlines = () => {
   // Clear page
   newsListContainer.innerHTML = "";
   headlinesContainer.innerHTML = "";
-  loader.style.display = 'block';
+  loader.style.display = "block";
+  totalResults.innerHTML = "";
 
-  let topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=&apiKey=${key}`;
+  let topHeadlinesUrl = `${newsAppApiBaseUrl}/getAPIResponse/topHeadlines`;
 
   fetch(topHeadlinesUrl)
     .then((res) => res.json())
     .then((data) => {
+      totalResults.innerHTML = `Total results: ${data.totalResults}`
+      console.log(data.totalResults)
       const headlinesContainer = document.getElementById("headlinesContainer");
       let articleCard = data.articles
-        .map((article) => {    
+        .map((article) => {
+
           return `
-      <div onclick="location.href ='${article.url}';" target="_blank" class="article-card">
+      <div onclick="window.open('${article.url}', '_blank')" class="article-card">
           <div class="img-wrapper">
-          <img src="${article.urlToImage || './no-image.jpeg'}" href=${article.url} alt="article-img" loading="lazy">
+          <img src="${article.urlToImage || "./no-image.jpeg"}" href=${article.url
+            } alt="article-img" loading="lazy">
           </div>
           <h3>${article.title}</h3>
-          <p>${article.description || '--Description not available!--'}</p>
-          <cite>${article.source.name}-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth()+1}-${new Date(article.publishedAt).getDate()}</cite>
+          <p>${article.description || "--Description not available!--"}</p>
+          <cite>${article.source.name}-${new Date(
+              article.publishedAt
+            ).getFullYear()}-${new Date(article.publishedAt).getMonth() + 1
+            }-${new Date(article.publishedAt).getDate()}</cite>
       </div>`;
-    })
-    .join("");
-      
-      loader.style.display = 'none';
-    
-      headlinesContainer.innerHTML = articleCard;
+        })
+        .join("");
 
-      
+      loader.style.display = "none";
+
+      headlinesContainer.innerHTML = articleCard;
     })
     .catch((error) => {
       console.log(error);
     });
-  };
-  
+};
+
 
 //  Headlines by category
 
 const getValue = (e) => {
-  
   // Clear page
   newsListContainer.innerHTML = "";
   headlinesContainer.innerHTML = "";
-  loader.style.display = 'block';
+  loader.style.display = "block";
 
-  let categoriesValue =  e.target.value;  
-  
-  let categoriesUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&category=${categoriesValue}&apiKey=${key}`
-  
+  let categoriesValue = e.target.value;
+
+  let categoriesUrl = `${newsAppApiBaseUrl}/getAPIResponse/category?category=${categoriesValue}`;
+  // `https://newsapi.org/v2/top-headlines?country=us&pageSize=30&category=${categoriesValue}&apiKey=${key}`
+
 
   fetch(categoriesUrl)
-  .then(response => response.json())
-  .then(data => {
-    let categoryCard = data.articles
-    .map((article) => {
-      return `
-  <div onclick="location.href ='${article.url}';" target="_blank" class="article-card">
+    .then((response) => response.json())
+    .then((data) => {
+      totalResults.innerHTML = `Total results: ${data.totalResults}`
+      let categoryCard = data.articles
+        .map((article) => {
+          return `
+  <div onclick="window.open('${article.url}', '_blank')" class="article-card">
       <div class="img-wrapper">
-      <img src="${article.urlToImage || './no-image.jpeg'}"  alt="article-img" loading="lazy">
+      <img src="${article.urlToImage || "./no-image.jpeg"
+            }"  alt="article-img" loading="lazy">
       </div>
       <h3>${article.title}</h3>
-      <p>${article.description}</p><cite style=>${article.source.name}-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth()+1}-${new Date(article.publishedAt).getDate()}</cite>
+      <p>${article.description}</p><cite style=>${article.source.name
+            }-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth() + 1
+            }-${new Date(article.publishedAt).getDate()}</cite>
   </div>`;
+        })
+        .join("");
+
+      loader.style.display = "none";
+      headlinesContainer.innerHTML = categoryCard;
     })
-    .join("");
-    
-    loader.style.display = 'none';
-    headlinesContainer.innerHTML = categoryCard;
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-}
-
-
-categories.addEventListener('change', getValue);
+const categories = document.getElementById("categories");
+categories.addEventListener("change", getValue);
 
 //
-
 
 // Get Search
 
@@ -94,69 +105,63 @@ const searchFrom = document.getElementById("searchFrom");
 const newsListContainer = document.getElementById("newsListContainer");
 
 const retriveSearch = (e) => {
-
   // Clear page
   newsListContainer.innerHTML = "";
   headlinesContainer.innerHTML = "";
-  loader.style.display = 'block';
+  loader.style.display = "block";
 
   // Set drop down value back to disabled
-  categories.value = "Select Category"
-
+  categories.value = "Select Category";
 
   e.preventDefault();
 
   let topic = searchInput.value;
-  let searchUrl = `https://newsapi.org/v2/everything?q=${topic}&pageSize=100&apiKey=${key}`;
+  let searchUrl = `${newsAppApiBaseUrl}/getAPIResponse/topic?topic=${topic}`;
+
+  // `https://newsapi.org/v2/everything?q=${topic}&pageSize=100&apiKey=${key}`
 
   fetch(searchUrl)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
+      internationalNumberFormat = new Intl.NumberFormat('en-GB')
+      totalResults.innerHTML = `Total results: ${internationalNumberFormat.format(data.totalResults)}`
       let searchList = data.articles
-        .map(article => {
+        .map((article) => {
           return `
-      <div onclick="location.href ='${article.url}';" target="_blank" class="search-list-item" >
-          <img src="${article.urlToImage || './no-image.jpeg'}" alt="article-img" loading="lazy"><cite style=>${article.source.name}-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth()+1}-${new Date(article.publishedAt).getDate()}</cite>
+      <div onclick="window.open('${article.url}', '_blank')" class="search-list-item" >
+          <img src="${article.urlToImage || "./no-image.jpeg"
+            }" alt="article-img" loading="lazy"><cite style=>${article.source.name
+            }-${new Date(article.publishedAt).getFullYear()}-${new Date(article.publishedAt).getMonth() + 1
+            }-${new Date(article.publishedAt).getDate()}</cite>
           <h2>${article.title}</h2>  
-      </div>`
+      </div>`;
         })
         .join("");
 
-      loader.style.display = 'none'; 
+      loader.style.display = "none";
       newsListContainer.innerHTML = searchList;
-
     });
 };
 
 searchFrom.addEventListener("submit", retriveSearch);
 //
 
-
 // Back to the top button
-window.addEventListener('scroll', () => {
-
-  let crolled;
+window.addEventListener("scroll", () => {
+  let scrolled;
   scrolled = window.scrollY;
-  const arrowTopBtn = document.getElementById('arrowTopBtn');
-  if(scrolled > 167) {
-    arrowTopBtn.style.visibility = 'visible'
+  const arrowTopBtn = document.getElementById("arrowTopBtn");
+  if (scrolled > 167) {
+    arrowTopBtn.style.visibility = "visible";
   } else {
-    arrowTopBtn.style.visibility = 'hidden'
+    arrowTopBtn.style.visibility = "hidden";
   }
-})
+});
 
-arrowTopBtn.addEventListener('click', () => {
-    document.documentElement.scrollTop = 0;
 
-})
+arrowTopBtn.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
 //
 
-
-// Loader 
-
-
-
-
-
-
-
+// Loader
